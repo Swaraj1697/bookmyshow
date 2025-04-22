ec65ddb2b8ab5787c3439cc8d9509549432174c9
Swaraj Mestry <swaraj.mestry@hettich.com>
2025-04-22 12:38:17 +0530

    Removed submodule and added client folder properly

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button, Form, Input } from 'antd'
+import { Link } from 'react-router-dom'
+
+function Login() {
+    return (
+        <>
+            <main className="App-header">
+                <h1>Login to BookMyShow</h1>
+                <section className="mw-500 text-center px-3"></section>
+                <Form layout="vertical">
+                    <Form.Item label="Email" htmlFor="email" name="email" className='d-block' rules={[{ required: true, message: 'Please input your email!' }, { type: "email", message: "Please enter a valid email" }]}>
+                        <Input id="email" type="text" placeholder="Enter your email" />
+                    </Form.Item>
+                    <Form.Item label="Password" htmlFor="password" name="password" className='d-block' rules={[{ required: true, message: 'Please input your password!' }]}>
+                        <Input id="password" type="password" placeholder="Enter your password" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            block
+                            htmlType="submit"
+                            style={{ fontSize: "1rem", fontWeight: "600" }}
+                        >
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <div>
+                    <p>
+                        New User ? <Link to="/register" >Register Here</Link>
+                    </p>
+                </div>
+
+            </main>
+        </>
+    )
+}
+
+export default Login
---
+++ client/src/pages/Register/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Button, Form, Input } from 'antd'
+import { Link } from 'react-router-dom'
+
+function Register() {
+    return (
+        <>
+            <main className="App-header">
+                <h1>Register to BookMyShow</h1>
+                <section className="mw-500 text-center px-6"></section>
+                <Form layout="vertical">
+                    <Form.Item label="Name" htmlFor="name" name="name" className='d-block' rules={[{ required: true, message: 'Please input your email!' }]}>
+                        <Input id="name" type="text" placeholder="Enter your email" />
+                    </Form.Item>
+                    <Form.Item label="Email" htmlFor="email" name="email" className='d-block' rules={[{ required: true, message: 'Please input your email!' }, { type: "email", message: "Please enter a valid email" }]}>
+                        <Input id="email" type="text" placeholder="Enter your email" />
+                    </Form.Item>
+                    <Form.Item label="Password" htmlFor="password" name="password" className='d-block' rules={[{ required: true, message: 'Please input your password!' }]}>
+                        <Input id="password" type="password" placeholder="Enter your password" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            block
+                            htmlType="submit"
+                            style={{ fontSize: "1rem", fontWeight: "600" }}
+                        >
+                            Register
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <div>
+                    <p>
+                        Already a User ? <Link to="/login" >Login here</Link>
+                    </p>
+                </div>
+
+            </main>
+        </>
+    )
+}
+
+export default Register
